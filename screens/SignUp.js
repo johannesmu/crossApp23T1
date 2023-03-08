b96a3d309d4cd9e6ff9b71ad0cb77eecc4546869
johannes <johannes.muljana@ait.nsw.edu.au>
2023-03-08 18:08:34 +1100

    simple form validation with controlled elemnts

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -3,7 +3,37 @@ import { useEffect, useState } from 'react'
 
 export function SignUpScreen ( props ) {
     const [ email, setEmail ] = useState("")
+    const [ validEmail, setValidEmail ] = useState(false)
     const [ password, setPassword ] = useState("")
+    const [ validPassword, setValidPassword ] = useState(false)
+    const [ validForm, setValidForm ] = useState( false)
+
+    useEffect( () => {
+        if( email.indexOf('@') > 0 ) {
+            setValidEmail( true )
+        }
+        else {
+            setValidEmail( false )
+        }
+    }, [email] )
+
+    useEffect( () => {
+       if( password.length >= 8 ) {
+            setValidPassword( true )
+       }
+       else {
+            setValidPassword( false )
+       }
+    }, [password])
+
+    useEffect( () => {
+        if( validEmail && validPassword ) {
+            setValidForm(true)
+        }
+        else {
+            setValidForm(false)
+        }
+    })
 
     return(
         <View style={ styles.page }>
@@ -11,7 +41,7 @@ export function SignUpScreen ( props ) {
             <View style={ styles.inputGroup }>
                 <Text>Email address</Text>
                 <TextInput 
-                    style={styles.input} 
+                    style={ (validEmail) ? styles.validInput : styles.input} 
                     placeholder="you@domain.com"
                     value={ email }
                     onChangeText={ (emailText) => setEmail(emailText) }
@@ -20,14 +50,17 @@ export function SignUpScreen ( props ) {
             <View style={ styles.inputGroup }>
                 <Text>Password</Text>
                 <TextInput 
-                    style={styles.input} 
+                    style={ (validPassword) ? styles.validInput : styles.input}  
                     placeholder="minimum 8 characters"
                     value={ password }
                     onChangeText={ (pwText) => setPassword(pwText) }
                     secureTextEntry={true}
                 />
             </View>
-            <TouchableOpacity style={ styles.button }>
+            <TouchableOpacity 
+                style={ (validForm) ? styles.button : styles.buttonDisabled }  
+                disabled={ (validForm) ? false : true}
+            >
                 <Text style={ styles.buttonText }>Sign up</Text>
             </TouchableOpacity>
         </View>
@@ -46,6 +79,14 @@ const styles = StyleSheet.create({
     input: {
         backgroundColor: "#ffffff",
         padding: 5,
+        borderWidth: 1,
+        borderColor: "#cccccc",
+    },
+    validInput: {
+        borderColor: "green",
+        borderWidth: 1,
+        backgroundColor: "#ffffff",
+        padding: 5,
     },
     inputGroup: {
         marginVertical: 5,
@@ -58,5 +99,10 @@ const styles = StyleSheet.create({
     buttonText: {
         color: "#ffffff",
         textAlign: "center"
+    },
+    buttonDisabled: {
+        backgroundColor: "#666666",
+        padding: 10,
+        marginVertical: 10,
     }
 })
