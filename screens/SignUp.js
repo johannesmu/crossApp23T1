93d9312c443e4089be34b297439445cb0a3ce3a9
johannes <johannes.muljana@ait.nsw.edu.au>
2023-03-08 17:27:11 +1100

    adding sign up form

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,9 +1,47 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
 
 export function SignUpScreen ( props ) {
     return(
-        <View>
-            <Text>Sign up Screen</Text>
+        <View style={ styles.page }>
+            <Text style={ styles.title }>Sign up for an account</Text>
+            <View style={ styles.inputGroup }>
+                <Text>Email address</Text>
+                <TextInput style={styles.input} />
+            </View>
+            <View style={ styles.inputGroup }>
+                <Text>Password</Text>
+                <TextInput style={styles.input} />
+            </View>
+            <TouchableOpacity style={ styles.button }>
+                <Text style={ styles.buttonText }>Sign up</Text>
+            </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    page: {
+        marginHorizontal: 60,
+    },
+    title: {
+        fontSize: 20,
+        textAlign: 'center',
+        marginTop: 20,
+    },
+    input: {
+        backgroundColor: "#ffffff",
+        padding: 5,
+    },
+    inputGroup: {
+        marginVertical: 5,
+    },
+    button: {
+        backgroundColor: "#000000",
+        padding: 10,
+        marginVertical: 10,
+    },
+    buttonText: {
+        color: "#ffffff",
+        textAlign: "center"
+    }
+})
